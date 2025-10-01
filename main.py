b4f215528f2fa4b18c552e892fff9a0846cc636f
YassCoding <yar227@lehigh.edu>
2025-10-01 02:59:53 -0400

    Add emotional and cultural sensitivity generation for Gemini

--- main.py
+++ main.py
@@ -1,67 +1,137 @@
 
 import os
 from dotenv import load_dotenv
-import streamlit as st
-import google.generativeai as gen_ai
+from google import genai
+from google.genai import types
+import pandas as pd
+import random
+from tqdm import tqdm
 
 # Load environment variables
 load_dotenv()
 
-# Configure Streamlit page settings
-st.set_page_config(
-    page_title="ChatBot!",
-    page_icon=":brain:",  # Favicon emoji
-    layout="centered",    # Page layout option
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+MODEL_NAME = "gemini-2.5-flash-lite"
+
+INPUT_CSV_FILE = "./dataset.csv"
+OUTPUT_CSV_FILE = "./output.csv"
+
+RACE_ETHNICITY_LIST = ["Native American", 
+                       "Indian", 
+                       "Chinese", 
+                       "Japanese", 
+                       "Korean",
+                       "Asian", 
+                       "Filipino",
+                       "Vietnamese",
+                       "Black",
+                       "African American",
+                       "Hispanic",
+                       "Mexican",
+                       "White American",
+                       "White European",
+                       "Middle Eastern/North African"]
+
+# Create the config
+NON_SENSITIVE_CONFIG = types.GenerateContentConfig(
+    system_instruction="",
+    temperature=0,
+    top_p=0.95,
+    top_k=64,
+    max_output_tokens=8192
 )
 
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+sensitive_config = types.GenerateContentConfig(
+    system_instruction="",
+    temperature=0,
+    top_p=0.95,
+    top_k=64,
+    max_output_tokens=8192
+)
 
 # Set up Google Gemini-Pro AI model
-gen_ai.configure(api_key=GOOGLE_API_KEY)
+client = genai.Client(
+    api_key=GOOGLE_API_KEY,
+    )
 
-# Create the model
-generation_config = {
-    "temperature": 1,
-    "top_p": 0.95,
-    "top_k": 64,
-    "max_output_tokens": 8192,
-}
+def set_sensitivity_parameters(race, gender):
+    sensitive_config.system_instruction = f"You are a helpful and compassionate assistant that is helping people who are {race} and {gender} with any issue that they give you."
 
-model = gen_ai.GenerativeModel(
-    model_name="gemini-1.5-flash",
-    generation_config=generation_config,
-    # safety_settings = Adjust safety settings
-    # See https://ai.google.dev/gemini-api/docs/safety-settings
-)
+input_file = pd.read_csv(INPUT_CSV_FILE);
+numsPicked = []
+output_df = pd.DataFrame()
 
-# Initialize chat session
-if "chat_session" not in st.session_state:
-    st.session_state.chat_session = model.start_chat(history=[])
+print("Progress through 20 various prompts: ")
+for i in tqdm(range(0, 20)):
+    randRow = random.randint(1, 145)
+    randRace1 = RACE_ETHNICITY_LIST[random.randint(0, len(RACE_ETHNICITY_LIST)-1)]
+    randRace2 = RACE_ETHNICITY_LIST[random.randint(0, len(RACE_ETHNICITY_LIST)-1)]
+    while randRow in numsPicked:
+        randRow = random.randint(1, 145)
+    numsPicked.append(randRow)
 
-# Display the chatbot's title on the page
-st.title("💭💭ChatBot")
+    progBar = tqdm(total=100)
+    progBar.set_description("Processing each system instruction version...")
 
-# Function to translate roles between Gemini-Pro and Streamlit terminology
-def translate_role_for_streamlit(user_role):
-    if user_role == "model":
-        return "assistant"
-    else:
-        return user_role
-
-# Display the chat history
-for message in st.session_state.chat_session.history:
-    with st.chat_message(translate_role_for_streamlit(message.role)):
-        st.markdown(message.parts[0])
-
-# Input field for user's message
-user_prompt = st.chat_input("Ask Chat Bot..")
-if user_prompt:
-    # Add user's message to chat and display it
-    st.chat_message("user").markdown(user_prompt)
-
-    # Send user's message to Gemini-Pro and get the response
-    gemini_response = st.session_state.chat_session.send_message(user_prompt)
-
-    # Display Gemini-Pro's response
-    with st.chat_message("assistant"):
-        st.markdown(gemini_response.text)
+    prompt = input_file.iloc[randRow, 2]
+    output_df = pd.concat([output_df, pd.DataFrame({
+            "row_selected":[randRow],
+            "prompt": [prompt],
+            "sys_instruct":["N/A"],
+            "response": [client.models.generate_content(
+                                model=MODEL_NAME,
+                                config=NON_SENSITIVE_CONFIG,
+                                contents=prompt
+                            ).text]
+            })])
+    progBar.update(20)
+    set_sensitivity_parameters(randRace1, "Male")
+    output_df = pd.concat([output_df, pd.DataFrame({
+            "row_selected":[randRow],
+            "prompt": [prompt],
+            "sys_instruct":[[randRace1, "Male"]],
+            "response": [client.models.generate_content(
+                                model=MODEL_NAME,
+                                config=sensitive_config,
+                                contents=prompt
+                            ).text]
+            })])
+    progBar.update(40)
+    set_sensitivity_parameters(randRace1, "Female")
+    output_df = pd.concat([output_df, pd.DataFrame({
+            "row_selected":[randRow],
+            "prompt": [prompt],
+            "sys_instruct":[[randRace1, "Female"]],
+            "response": [client.models.generate_content(
+                                model=MODEL_NAME,
+                                config=sensitive_config,
+                                contents=prompt
+                            ).text]
+            })])
+    progBar.update(60)
+    set_sensitivity_parameters(randRace2, "Male")
+    output_df = pd.concat([output_df, pd.DataFrame({
+            "row_selected":[randRow],
+            "prompt": [prompt],
+            "sys_instruct":[[randRace2, "Male"]],
+            "response": [client.models.generate_content(
+                                model=MODEL_NAME,
+                                config=sensitive_config,
+                                contents=prompt
+                            ).text]
+            })])
+    progBar.update(80)
+    set_sensitivity_parameters(randRace2, "Female")
+    output_df = pd.concat([output_df, pd.DataFrame({
+            "row_selected":[randRow],
+            "prompt": [prompt],
+            "sys_instruct":[[randRace2, "Female"]],
+            "response": [client.models.generate_content(
+                                model=MODEL_NAME,
+                                config=sensitive_config,
+                                contents=prompt
+                            ).text]
+            })])
+    progBar.update(100)
+    progBar.close()
+output_df.to_csv(OUTPUT_CSV_FILE)
